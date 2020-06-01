c859e5c7ba63d904de3de90b6ceeb0cf29161160
Irrational Pi <pinowudi@gmail.com>
2020-06-01 12:49:26 +0000

    disabled wifi monitoring. will pursue using kismet api pulls for the same information as that platform is a bit more mature in monitoring.

--- src/eavesdropping/start_all.sh
+++ src/eavesdropping/start_all.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 while true; do
-	./wifi_scanner.py &
-	export scanner_pid=$!
+	echo "start"
+#	./wifi_scanner.py &
+#	export scanner_pid=$!
 	./pimotion.py &
 	export camera_pid=$!
 	./piaudio.py > /dev/null 2>&1 &
 	export audio_pid=$!
 
 	sleep 60
-	kill $scanner_pid
+#	kill $scanner_pid
 	kill $camera_pid
 	kill $audio_pid
-	echo("time up")
+	echo "time up"
 
 	./renaming.py
 #	./file_transfer.py
